ab7f8f3651486d23e6bbd8f2c9cca3c6f93f3881
Krzysztof Kowalczyk <k.kowaalczyk@gmail.com>
2018-01-11 13:13:19 +0100

    WIP forms, test with a sample view and seeded DB

--- liga/forms.py
+++ liga/forms.py
@@ -1,17 +1,43 @@
 from django import forms
 
+# Form rendered for each joinable tournament on index page
+# TODO: This should only show submit button - check before creating other forms
+# action: post request to create_player
 class JoinTournamentForm(forms.Form):
+    hidden_tournament_id_field = forms.HiddenInput(
+        forms.IntegerField(
+            empty_value=-1,
+            label='THIS SHOULD BE HIDDEN !!!',
+            coerce=lambda  x : -1,
+            choices=(-1, -1)
+    ))
+    hidden_accept_field = forms.HiddenInput(
+        forms.TypedChoiceField(
+            empty_value=True,
+            label='THIS SHOULD BE HIDDEN !!!',
+            coerce=lambda x: 'True',
+            choices=(True, 'Yes')
+    ))
+
+    def set_data(self, team_id):
+        self.hidden_tournament_id_field.coerce = lambda x : team_id
+        self.hidden_tournament_id_field.empty_value = team_id
+        self.hidden_tournament_id_field.choices = (team_id, team_id)
 
 
+# action: post request to create_team/:tournament_id
 class CreateTeamForm(forms.Form):
     # TODO
 
+# action: post request to update_team
 class UpdateTeamForm(forms.Form):
     # TODO
 
+# action: post request to invite_player_to_team
 class InviteToTeamForm(forms.Form):
     # TODO
 
+# action: post request to request_team
 class RequestTeamForm(forms.Form):
     # TODO
 
@@ -25,13 +51,21 @@ class ManagePlayerInviteForm(forms.Form):
         widget=forms.RadioSelect
     )
 
-    def set_data(self, team_name, player_invite_id):
+    def set_data(self, team_name):
         self.accept_invite_field.label = "Do You want to join {}?".format(team_name)
-        # TODO: Finish from here
 
 
 class ManageTeamRequestForm(forms.Form):
-    # TODO
+    accept_request_field = forms.TypedChoiceField(
+        empty_value=False,
+        label='DATA NOT SET !!!',
+        coerce=lambda x: x == 'True',
+        choices=((False, 'No'), (True, 'Yes')),
+        widget=forms.RadioSelect
+    )
+
+    def set_data(self, player_name):
+        self.accept_invite_field.label = "Do You want to add {} to Your team?".format(player_name)
 
 class CreateMatchForm(forms.Form):
     # TODO

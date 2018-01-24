2d737d009c656a97837b006a67749162c56abe43
Krzysztof Kowalczyk <k.kowaalczyk@gmail.com>
2018-01-24 02:20:34 +0100

    fix urls

--- liga/urls.py
+++ liga/urls.py
@@ -1,24 +1,29 @@
 from django.urls import path
 
 from . import views
+
+
 urlpatterns = [
     path('', views.index, name='index'),
     path('tournament/<int:tournament_id>/',
          views.tournament,
          name='tournament'),
-    path('tournament/<int:tournament_id>/players/create',
+    path('tournament/<int:tournament_id>/players/create/',
          views.create_player,
          name='join-tournament'),
-    path('tournament/<int:tournament_id>/teams/<int:team_id>',
-         views.tournament,
+    path('tournament/<int:tournament_id>/teams/<int:team_id>/',
+         views.team,
          name='team'),
-    path('tournament/<int:tournament_id>/teams/create',
+    path('tournament/<int:tournament_id>/teams/create/',
          views.create_team,
          name='create-team'),
-    path('tournament/<int:tournament_id>/teams/<int:team_id>/invitations/create',
+    path('tournament/<int:tournament_id>/invites/create/',
          views.create_player_invite,
          name='create-player-invite'),
-    path('tournament/<int:tournament_id>/matches/<int:match_id>',
+    path('tournament/<int:tournament_id>/requests/create/',
+         views.create_team_requst,
+         name='create-team-request'),
+    path('tournament/<int:tournament_id>/matches/<int:match_id>/',
          views.match,
          name='match'),
 ]
